a398942756ac85d80066d100821d5e24adbaed20
evanca <33544598+evanca@users.noreply.github.com>
2018-09-05 12:02:50 +0300

    Create an infinite scrolling ListView

--- lib/main.dart
+++ lib/main.dart
@@ -5,32 +5,70 @@ void main() => runApp(
     MyApp()); // The main method specifies fat arrow (=>) notation. Use fat arrow notation for one-line functions or methods.
 
 class MyApp extends StatelessWidget {
-  // The app extends StatelessWidget which makes the app itself a widget. In Flutter, almost everything is a widget, including alignment, padding, and layout.
   @override
   Widget build(BuildContext context) {
-    // A widget’s main job is to provide a build() method that describes how to display the widget in terms of other, lower level widgets.
     return MaterialApp(
-      // This example creates a Material app. Material is a visual design language that is standard on mobile and the web. Flutter offers a rich set of Material widgets.
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        // The Scaffold widget, from the Material library, provides a default app bar, title, and a body property that holds the widget tree for the home screen. The widget subtree can be quite complex.
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
 }
 
 class RandomWordsState extends State<RandomWords> {
   // This indicates that we’re using the generic State class specialized for use with RandomWords. Most of the app’s logic and state resides.
+
+  final _suggestions = <
+      WordPair>[]; // Add a _suggestions list to the RandomWordsState class for saving suggested word pairings.
+  final _biggerFont = const TextStyle(
+      fontSize:
+          18.0); // Add a biggerFont variable for making the font size larger.
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body:
+          _buildSuggestions(), // Use _buildSuggestions(), rather than directly calling the word generation library.
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        // The itemBuilder callback is called once per suggested word pairing,
+        // and places each suggestion into a ListTile row.
+        // For even rows, the function adds a ListTile row for the word pairing.
+        // For odd rows, the function adds a Divider widget to visually
+        // separate the entries. Note that the divider may be difficult
+        // to see on smaller devices.
+        itemBuilder: (context, i) {
+          // Add a one-pixel-high divider widget before each row in theListView.
+          if (i.isOdd) return Divider();
+
+          // The syntax "i ~/ 2" divides i by 2 and returns an integer result.
+          // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+          // This calculates the actual number of word pairings in the ListView,
+          // minus the divider widgets.
+          final index = i ~/ 2;
+          // If you've reached the end of the available word pairings...
+          if (index >= _suggestions.length) {
+            // ...then generate 10 more and add them to the suggestions list.
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    // This function displays each new pair in a ListTile.
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
 
